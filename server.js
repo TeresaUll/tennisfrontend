1918f294365c8bbf9cfa1e12cac56b6cbe6d27c8
TeresaUll <tbonetcosta@gmail.com>
2022-09-05 21:00:08 +0200

    netlify funciona

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/frontend-tennis'));
-
-app.listen(process.env.PORT || 8080);
